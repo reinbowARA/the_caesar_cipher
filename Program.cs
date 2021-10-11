365dc8b77826fb26b4bde7ca0aac2f856a3d2c49
Roman_Ablyazov <reinbowARA@yandex.ru>
2021-10-11 09:36:47 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,58 +1,39 @@
 ﻿using System;
+using System.Text;
 using static System.Console;
 
 namespace the_caesar_cipher
 {
-    public class CaesarCipher
-    {
-        //символы латинской азбуки
-        const string ABC = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-
-        private string CodeEncode(string text, int k)
-        {
-            //добавляем в алфавит маленькие буквы
-            var fullABC =ABC +ABC.ToLower();
-            var letterQty = fullABC.Length;
-            var retVal = "";
-            for (int i = 0; i < text.Length; i++)
-            {
-                var c = text[i];
-                var index = fullABC.IndexOf(c);
-                if (index < 0)
-                {
-                    //если символ не найден, то добавляем его в неизменном виде
-                    retVal += c.ToString();
-                }
-                else
-                {
-                    var codeIndex = (letterQty + index + k) % letterQty;
-                    retVal += fullABC[codeIndex];
-                }
-            }
-            return retVal;
-        }
-    //шифрование текста
-    public string Encrypt(string plainMessage, int key)
-        => CodeEncode(plainMessage, key);
-
-    //дешифрование текста
-    public string Decrypt(string encryptedMessage, int key)
-        => CodeEncode(encryptedMessage, -key);
-
-    }
     class Program
     {
-
-        static void Main(string[] args)
+        static StringBuilder Decrypt(StringBuilder ciphertext,int key)
         {
-            var cipher = new CaesarCipher();
-            Write("Введите текст: ");
-            var message = ReadLine();
-            Write("Введите ключ: ");
-            var secretKey = Convert.ToInt32(ReadLine());
-            var encryptedText = cipher.Encrypt(message, secretKey);
-            WriteLine("Зашифрованное сообщение: {0}", encryptedText);
-            WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText, secretKey));
+            StringBuilder plaintext = new StringBuilder(ciphertext.ToString());
+            
+            for (int index=0; index < plaintext.Length; index++)
+            {
+                if (ciphertext[index] != ' ')
+                {
+                    int character =
+                    (((ciphertext[index]+26-'A')-key)%26)+'A';
+                    plaintext[index] = (char)character;
+                }
+            }
+            return plaintext;
+        }
+        static void Main(string[] args)
+        {   
+            StringBuilder ciphertext = new StringBuilder();
+            Write("Введте ваше сообщение: ");
+            ciphertext.Append(ReadLine());
+            //последовательно проверять все пространство ключей
+            for (int testkey=1; testkey <= 25; testkey++)
+            {   
+                StringBuilder plaintext = Decrypt(ciphertext, testkey);
+                Console.WriteLine("Сдвиг на {0,2}: Полученный текст: {1,2}",
+                              testkey, plaintext);   
+                              
+            }
         }
     }
 }
